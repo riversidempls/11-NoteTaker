75eb305080f11a9455c9c7bb7de1d38d6838eebe
Conor Donnelly <donn0021@umn.edu>
2022-11-15 17:19:48 -0600

    Added GET route and POST routes to server.js

--- server.js
+++ server.js
@@ -1,22 +1,77 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+// Helper method for generating unique ids
+const uuid = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/send', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-);
+// GET request for notes
+app.get('/api/notes', (req, res) => {
+    // Send a message to the client
+    res.json(`${req.method} request received to get notes`);
 
-app.get('/routes', (req, res) =>
-    res.sendFile(path.join(__dirname, 'public/routes.html'))
-);
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get reviews`);
+});
 
-app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
-);
+// POST request to add a new note
+app.post('/api/note', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+
+    // Destructuring assignment for the items in req.body
+    const { title, text, id } = req.body;
+
+    // If all the required properties are present
+    if (title && text && id) {
+        // Variable for the object we will save
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+
+        // Convert the data to a string so we can save it
+        const noteString = JSON.stringify(newNote);
+
+        // Write the string to a file
+        fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+            err
+                ? console.error(err)
+                : console.log(
+                    `Review for ${newNote.title} has been written to JSON file`
+                )
+        );
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in saving note!');
+    }
+});
+
+// // Below Not Used for Notes App
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+// app.get('/send', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+// );
+
+// app.get('/routes', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'public/routes.html'))
+// );
+
+// app.listen(PORT, () =>
+//     console.log(`Example app listening at http://localhost:${PORT}`)
+// );
